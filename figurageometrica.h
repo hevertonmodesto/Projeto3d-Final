42f4c91f2782c4e0866c6c5ae3d7571f0e3ef672
hevertonmodesto <105319358+hevertonmodesto@users.noreply.github.com>
2022-07-21 11:21:19 -0300

    Update figurageometrica.h

--- figurageometrica.h
+++ figurageometrica.h
@@ -2,16 +2,18 @@
 #define FIGURAGEOMETRICA_H
 #include "sculptor.h"
 
-
+/*classe abstrata: a Classe FiguraGeometrica se trata de uma classe abstrata, quando não se diz o tipo de figura geometrica, o conceito é imaginário
+ja que estão incerridas nela varias formas geométrica, ou seja, um classe geral para os figuras*/
 class FiguraGeometrica
 {
 public:
 
-    FiguraGeometrica();
+    FiguraGeometrica(); //construtor
 
-    virtual void draw(Sculptor &t) = 0;
+    virtual void draw(Sculptor &t) = 0; /*O programa vai identificar como Função virtual pura, a função execurara o metodo de subclasses,
+    da propria classe  concreta de FiguraGeométrica*/
 
-    virtual ~FiguraGeometrica();
+    virtual ~FiguraGeometrica(); //destrutor
 };
 
 #endif // FIGURAGEOMETRICA_H
